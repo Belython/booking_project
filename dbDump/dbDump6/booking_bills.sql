b368b2609a27254f480c358c432e257f875bd878
Dzmitry Kanarski <belython@gmail.com>
2016-11-26 21:12:56 +0200

    Restore

--- dbDump/dbDump6/booking_bills.sql
+++
@@ -1,59 +0,0 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win64 (x86_64)
---
--- Host: localhost    Database: booking
--- ------------------------------------------------------
--- Server version	5.6.31-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `bills`
---
-
-DROP TABLE IF EXISTS `bills`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `bills` (
-  `BILL_ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `USER_ID` bigint(20) NOT NULL,
-  `TOTAL_PERSONS` int(11) NOT NULL,
-  `CHECK_IN_DATE` bigint(20) NOT NULL,
-  `CHECK_OUT_DATE` bigint(20) NOT NULL,
-  `ROOM_ID_LIST` blob NOT NULL,
-  `PAYMENT_AMOUNT` int(11) NOT NULL,
-  `BILL_STATUS` varchar(45) NOT NULL,
-  PRIMARY KEY (`BILL_ID`),
-  KEY `USER_BILL_KEY_idx` (`USER_ID`),
-  CONSTRAINT `USER_BILL_KEY` FOREIGN KEY (`USER_ID`) REFERENCES `users` (`USER_ID`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE=InnoDB AUTO_INCREMENT=16 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `bills`
---
-
-LOCK TABLES `bills` WRITE;
-/*!40000 ALTER TABLE `bills` DISABLE KEYS */;
-INSERT INTO `bills` VALUES (1,1,1,111111111,1111111111,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0	x',1250,'paid'),(2,2,1,1111111111,1111111111,'2',1350,'paid'),(3,3,1,1111111111,1111111111,'3',2000,'notPaid'),(4,4,1,1111111111,1111111111,'5',3000,'notPaid'),(5,5,1,1111111111,1111111111,'4',1500,'notPaid'),(6,1,1,1461963600000,1466370000000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0	x',35700,'paid'),(7,1,1,1435611600000,1437339600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0x',28000,'paid'),(8,1,1,1435611600000,1437339600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0x',26000,'refused'),(9,1,1,1435611600000,1437339600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0x',28000,'refused'),(10,1,1,1435611600000,1437339600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0x',28000,'refused'),(11,1,1,1435611600000,1437339600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0x',28000,'refused'),(12,1,1,1435611600000,1437339600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0x',28000,'refused'),(13,1,1,1435698000000,1436907600000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0	sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0\rsq\0~\0\0\0\0\0\0\0\0x',35000,'paid'),(14,1,1,1476133200000,1476910800000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0\nx',7200,'notPaid'),(15,1,3,1476046800000,1476910800000,'¨\Ì\0sr\0java.util.ArrayListxÅ\“ô\«aù\0I\0sizexp\0\0\0w\0\0\0sr\0java.lang.Long;ã\‰êÃè#\ﬂ\0J\0valuexr\0java.lang.NumberÜ¨ïî\‡ã\0\0xp\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0sq\0~\0\0\0\0\0\0\0\0\nx',14000,'notPaid');
-/*!40000 ALTER TABLE `bills` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-10-26 17:44:21
--- dbDump/dump4/BOOKING_rooms_types.sql
+++
@@ -1,55 +0,0 @@
--- MySQL dump 10.13  Distrib 5.7.9, for Win32 (AMD64)
---
--- Host: localhost    Database: BOOKING
--- ------------------------------------------------------
--- Server version	5.6.31-log
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `rooms_types`
---
-
-DROP TABLE IF EXISTS `rooms_types`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `rooms_types` (
-  `ROOM_TYPE_ID` bigint(20) NOT NULL AUTO_INCREMENT,
-  `ROOM_TYPE` varchar(15) NOT NULL,
-  `MAX_PERSONS` int(11) NOT NULL,
-  `PRICE_PER_NIGHT` bigint(20) NOT NULL,
-  `FACILITIES` blob NOT NULL,
-  `ROOM_TYPE_STATUS` varchar(45) NOT NULL,
-  PRIMARY KEY (`ROOM_TYPE_ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=17 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `rooms_types`
---
-
-LOCK TABLES `rooms_types` WRITE;
-/*!40000 ALTER TABLE `rooms_types` DISABLE KEYS */;
-INSERT INTO `rooms_types` VALUES (1,'Standard Twin',2,200,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safex','1'),(2,'Standard Double',2,300,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safex','1'),(3,'Family',3,400,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safex','1'),(4,'Standard Single',1,500,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safex','1'),(5,'Huge',4,600,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safex','1'),(6,'Family',3,400,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0showert\0wi-fit\0safet\0soapx','active'),(7,'Family',5,400,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0showert\0wi-fit\0safet\0soapx','active'),(8,'Huge',5,350,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safet\0soapx','active'),(9,'Huge',2,350,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safet\0soapx','active'),(10,'Huge',6,750,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safet\0profitt\0soapx','active'),(11,'Standard Twin',6,750,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safet\0profitt\0soapx','active'),(12,'Standard Twin',6,750,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0wi-fit\0safet\0profitx','active'),(13,'Standard Twin',4,250,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0toilett\0wi-fit\0safet\0profitx','active'),(14,'Huge',2,350,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0toilett\0wi-fit\0safet\0profitx','active'),(15,'Huge',2,450,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0toilett\0wi-fit\0safet\0profitx','active'),(16,'Family',3,500,'¬\í\0sr\0java.util.HashSetºD…•–¸·4\0\0xpw\0\0\0?@\0\0\0\0\0t\0toilett\0wi-fit\0basikt\0safet\0profitx','active');
-/*!40000 ALTER TABLE `rooms_types` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2016-10-15 16:05:53
